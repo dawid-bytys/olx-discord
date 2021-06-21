c1699a0fd9c71b5c4a6bdb91c72a3b93502c3f83
salvia-dev <salviadev@protonmail.com>
2021-06-21 07:47:27 +0200

    repair app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Discord from "discord.js";
 import { fetchOlx } from "./fetchData.js";
 
-const bot = new Discord.Client; // initialize the bot
+const bot = new Discord.Client(); // initialize the bot
 
 // GLOBAL VARIABLES
 const prefix = "!";
@@ -9,60 +9,56 @@ let currentOffer = "wacom"; // default value is "wacom"
 let latestOffer = "";
 
 bot.on("ready", () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+  console.log(`Logged in as ${bot.user.tag}!`);
 
-    const channel = bot.channels.cache.get("833649510109544508");
+  const channel = bot.channels.cache.get("833649510109544508");
 
-    setInterval(async () => {
-        try {
-            if (currentOffer) {
-                const { title, price, image, url } = await fetchOlx(`https://www.olx.pl/oferty/q-${currentOffer}/?search%5Border%5D=created_at%3Adesc`);
+  setInterval(async () => {
+    if (!currentOffer) return;
 
-                if (title && url !== latestOffer) {
-                    const offerEmbed = new Discord.MessageEmbed()
-                        .setColor("#57E5DB")
-                        .setTitle(title)
-                        .setURL(url)
-                        .setDescription(`**${price}**`)
-                        .setThumbnail(image)
+    const { title, price, image, url } = await fetchOlx(
+      `https://www.olx.pl/oferty/q-${currentOffer}/?search%5Border%5D=created_at%3Adesc`
+    );
 
-                    channel.send(offerEmbed);
-                }
-                
-                latestOffer = url;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    }, 5000);
-})
+    if (!title || url === latestOffer) return;
+
+    const offerEmbed = new Discord.MessageEmbed()
+      .setColor("#57E5DB")
+      .setTitle(title)
+      .setURL(url)
+      .setDescription(`**${price}**`)
+      .setThumbnail(image);
+
+    channel.send(offerEmbed);
+
+    latestOffer = url;
+  }, 3000);
+});
 
 bot.on("message", message => {
-    if (message.author.bot) return;
+  if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-        switch (command) {
-            case "offer":
-                if (args.length > 0) {
-                    currentOffer = args;
-                    message.channel.send(`New offer: ${currentOffer}`);
-                }
-                break;
-            case "stop":
-                currentOffer = "";
-                message.channel.send(`Action has successfully stopped.`);
-                break;
-            case "latest":
-                message.channel.send(latestOffer);
-                break;
-            default:
-                message.channel.send("Command not found.")
-                break;
-        }
-    }
-})
+  switch (command) {
+    case "offer":
+      if (args.length <= 0) return;
+
+      currentOffer = args;
+      message.channel.send(`New offer: ${currentOffer}`);
+      break;
+    case "stop":
+      currentOffer = "";
+      message.channel.send(`Action has successfully stopped.`);
+      break;
+    case "latest":
+      message.channel.send(latestOffer);
+      break;
+    default:
+      message.channel.send("Command not found.");
+      break;
+  }
+});
 
 bot.login(process.env.BOT_TOKEN); // login the bot
